f2a1a14bc5698162b8f0493bdfe248300610d3cb
LENOVO <LENOVO@10.0.0.119>
2023-09-27 11:03:13 +0530

    first commit

---
+++ project1/src/com/tnsif/project/first.java
@@ -0,0 +1,10 @@
+package com.tnsif.project;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Aparna");
+	}
+
+}
