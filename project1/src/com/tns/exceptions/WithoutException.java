1233b20524ea97ffad29d9ffb35b461cc2c7e1ec
LENOVO <LENOVO@DESKTOP-AMJJJRC>
2023-10-11 17:46:34 +0530

    Exception Handling

---
+++ project1/src/com/tns/exceptions/WithoutException.java
@@ -0,0 +1,14 @@
+package com.tns.exceptions;
+
+public class WithoutException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int d=0;
+		int a=30/d;
+		System.out.println("will it be printed");
+
+	}
+
+}
